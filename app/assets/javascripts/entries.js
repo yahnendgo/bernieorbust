34f9a97ecd53ee8b452377c9de4c599fdd20641b
Donovan McRae <d_mcrae@ymail.com>
2016-07-01 15:42:46 -0400

    adding form functionality; adding default value to ans2 (did not select option 2)

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -1,16 +1,21 @@
 $(document).ready(function(){
-	(console).log('ready');
+	console.log('ready');
+
+	// disables form submission with enter key
+	$('#new_entry').on('keyup keypress', function(e){
+		var keycode = e.keyCode || e.which;
+		if(keycode === 13){
+			e.preventDefault();
+			return false;
+		}
+	});
 
 	$('.q-state').hide();
 	$('.q-email').hide();
 	$('.q-quest-1').hide();
 	$('.q-quest-2').hide();
-	$('.q-actions').hide();
-
-	function nextField(x, y){
-		x.hide('slide', {direction: 'left'}, 1000);
-		y.show();
-	}
+	$('.q-submit').hide();
+	$('.q-thanks').hide();
 
 	$('.q-name-next').on('click', function(){
 		$('.q-name').hide();
@@ -25,8 +30,18 @@ $(document).ready(function(){
 	$('.q-email-next').on('click', function(){
 		$('.q-email').hide();
 		$('.q-quest-1').show();
+	});
 
-		// set variable answer to gets.chomp; if answer is option 2, show question 2 and submit, else show submit button
+	$('.q1-next').on('click', function(){
+			if($('.q-quest-1 input[type="radio"]:checked').val() == "Vote Green only if it's Bernie"){
+				$('.q-quest-1').hide();
+				$('.q-quest-2').show();
+				$('.q-submit').show();
+			}else{
+				$('.q-quest-1').hide();
+				$('.q-thanks').show();
+				$('.q-submit').show();
+			}
 	});
 
 });
--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,14 +7,21 @@ class EntriesController < ApplicationController
 	def create
 		@entry = Entry.new(entry_params)
 
-		if @entry.save
-			# render thank you div/ redirect to root path
+		if @entry
+			if @entry.ans2 === nil
+				@entry.ans2 = "Did not select option 2"
+			end	
+			@entry.save
+		else
+			print "not saved"
 		end
+
+		redirect_to new_entry_path
 	end
 
 	private
 
 	def entry_params
-		params.require(:entry).permit(:fname, :lname, :state, :email, :ans1, :ans2)
+		params.require(:entry).permit(:fname, :lname, :email, :ans1, :ans2)
 	end
 end
